00895f0d559da3b5e2fb0261e8ed83b94c988fa0
KJH <gh7052@gmail.com>
2024-04-12 21:44:57 +0900

    Add : NetworkManager 추가

---
+++ GithubProfile/Model/NetworkManager.swift
@@ -0,0 +1,62 @@
+//
+//  NetworkManager.swift
+//  GithubProfile
+//
+//  Created by 김정호 on 4/12/24.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkManager {
+    let url = "https://api.github.com/users/"
+    
+    // (1) URLSession
+    func fetchUserProfile(userName: String, completion: @escaping ((Result<ProfileDTO, Error>) -> Void)) {
+        guard let url = URL(string: "\(self.url)\(userName)") else {
+            completion(.failure(NSError(domain: "URL 변환에 실패했어요.", code: 401)))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data else {
+                completion(.failure(NSError(domain: "Data가 없어요.", code: 402)))
+                return
+            }
+            
+            do {
+                let profile = try JSONDecoder().decode(ProfileDTO.self, from: data)
+                completion(.success(profile))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // (2) Alamofire
+    func fetchUserRepository(userName: String, page: Int, completion: @escaping ((Result<[RepositoryDTO], Error>) -> Void)) {
+        guard let url = URL(string: "\(self.url)\(userName)?page=\(page)") else {
+            completion(.failure(NSError(domain: "URL 변환에 실패했어요.", code: 401)))
+            return
+        }
+        
+        AF.request(url, method: .get).responseDecodable(of: [RepositoryDTO].self) { response in
+            switch response.result {
+            case .success(let repositories):
+                completion(.success(repositories))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
