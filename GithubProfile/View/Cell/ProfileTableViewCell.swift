7c1b462f42b690f99205eb7613f9d1f8a30186b2
KJH <gh7052@gmail.com>
2024-04-12 17:41:58 +0900

    Add : 프로필 화면 UI 추가

---
+++ GithubProfile/View/Cell/ProfileTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  ProfileTableViewCell.swift
+//  GithubProfile
+//
+//  Created by 김정호 on 4/12/24.
+//
+
+import UIKit
+
+final class ProfileTableViewCell: UITableViewCell {
+    
+    // MARK: - properties
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .clear
+        return imageView
+    }()
+    
+    let name: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    let nickname: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private lazy var nameStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [name, nickname])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .leading
+        stackView.spacing = 2
+        return stackView
+    }()
+    
+    // MARK: - methods
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraints() {
+        self.contentView.addSubview(profileImageView)
+        self.contentView.addSubview(nameStackView)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            profileImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        
+            nameStackView.centerYAnchor.constraint(equalTo: self.profileImageView.centerYAnchor),
+            nameStackView.leadingAnchor.constraint(equalTo: self.profileImageView.trailingAnchor, constant: 10)
+        ])
+    }
+}
